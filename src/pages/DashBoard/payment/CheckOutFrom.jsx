41bab49dbbff5c97ab83fdaef9f874a675f69bfc
sabitameem <sabitameem578@gmail.com>
2023-06-03 21:48:51 +0530

    added card number and error

--- src/pages/DashBoard/payment/CheckOutFrom.jsx
+++ src/pages/DashBoard/payment/CheckOutFrom.jsx
@@ -1,14 +1,16 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckOutFrom = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
   const handleSubmit = async (event) => {
     // Block native form submission.
     event.preventDefault();
-    if(!stripe || !elements){
-        return;
+    if (!stripe || !elements) {
+      return;
     }
 
     const card = elements.getElement(CardElement);
@@ -16,30 +18,51 @@ const CheckOutFrom = () => {
     if (card == null) {
       return;
     }
+    const { error } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("error", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      //console.log('payment method', paymentMethod)
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form className="w-2/3 m-8 " onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <div className="flex justify-center mt-16">
+          <button
+            className="w-[421px] py-2 rounded-lg  bg-custom-color text-white"
+            type="submit"
+            disabled={!stripe}
+          >
+            Pay
+          </button>
+        </div>
+      </form>
+      {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+    </>
   );
 };
 
